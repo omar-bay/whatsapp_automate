22de28395a6add9482dc3feadcac282b9102e33c
omar-bay <omarmb2002@gmail.com>
2022-01-06 21:45:10 +0200

    new file:   whatsapp_sender.py

---
+++ whatsapp_sender.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from time import sleep
+
+# driver = webdriver.Chrome()
+driver = webdriver.Firefox()
+# driver = webdriver.Edge()
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group : ')
+filepath = input('Enter your filepath : ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+attachment_box = driver.find_element_by_xpath('//div[@title = "Attach"]')
+attachment_box.click()
+
+image_box = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+image_box.send_keys(filepath)
+
+sleep(3)
+
+send_button = driver.find_element_by_xpath('//span[@data-icon="send"]')
+send_button.click()
+
+# https://www.youtube.com/watch?v=x85ba0VYsYo
+# https://pypi.org/project/selenium/
+# the driver.exe should be with your python.exe
